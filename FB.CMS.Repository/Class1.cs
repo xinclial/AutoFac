e96fabfc0d58f161ef83cef01bde11efe6c48bfc
程伟 <chengwei10@centaline.com.cn>
2017-05-16 14:54:49 +0800

    add

---
+++ FB.CMS.Repository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FB.CMS.Repository
+{
+    public class Class1
+    {
+    }
+}
